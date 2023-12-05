8b874c99a35e7e2c6b640f62496a69ae857135f3
Nicholas Jeremy <145427187+Senicccc@users.noreply.github.com>
2023-12-05 21:54:40 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <iomanip> // input output manipulator
 
 using namespace std;
 
@@ -219,18 +220,26 @@ void menu2() {
     switch (pilih)
     {
     case 1:
+        bersih();
         cari();
+        bersih();
         break;
     case 2:
+        bersih();
         tambah();
+        bersih();
         return menu2();
         break;
     case 3:
+        bersih();
         tampil();
+        bersih();
         cout << "Kembali ke menu? (y/n)";
         cin >> balik;
         if (balik == 'y')
         {
+            kepala();
+            bersih();
             return menu2();
         }
         break;
@@ -247,9 +256,158 @@ void menu2() {
     }
 }
 
+void terHapus();
+void terEdit();
+void tidakValid();
+
 void cari() {
-    // Implementation of the 'cari' function
+    kepala();
+    bool ketemu = false;
+    int ind = 0;
+    char hapus, edit;
+    string key;
+    cout << "||                      PENCARIAN DATA                      ||\n";
+    cout << "==============================================================\n";
+    cout << "Cari data: ";
+    cin >> key;
+    for (int i = 0; i < dex; i++)
+    {
+        if (key == karyawan[i].nama)
+        {
+            bersih();
+            kepala();
+            cout << "Data "
+                 << "\"" << karyawan[i].nama << "\""
+                 << " ditemukan\n\n";
+            cout << setw(5) << left << "No"
+                 << setw(13) << left << "Nama"
+                 << setw(8) << left << "Gender"
+                 << setw(5) << left << "Umur"
+                 << setw(13) << left << "Alamat"
+                 << setw(10) << left << "Jabatan"
+                 << setw(10) << left << "Status" << endl;
+            cout << "=============================================================\n";
+
+            if (karyawan[i].nama != "")
+            {
+                cout << setw(5) << left << i + 1 << setw(13) << left;
+                cout << karyawan[i].nama << setw(8) << left;
+                cout << karyawan[i].jk << setw(5) << left;
+                cout << karyawan[i].umur << setw(13) << left;
+                cout << karyawan[i].alamat << setw(10) << left;
+                cout << karyawan[i].jabatan << setw(10) << left;
+                cout << karyawan[i].status << endl;
+            }
+            ind = i;
+            ketemu = true;
+        }
+    }
+    if (ketemu == true)
+    {
+        int pilih;
+        cout << "\n=============================================================\n";
+        cout << "[1] Hapus Data\n";
+        cout << "[2] Edit Data\n";
+        cout << "[3] Kembali ke Menu Pilihan\n";
+        cout << "Pilihan: ";
+        cin >> pilih;
+        switch (pilih)
+        {
+        case 1:
+//Menghapus Data
+            bersih();
+            kepala();
+            cout << "Hapus data? (y/n)";
+            cin >> hapus;
+            if (hapus == 'y')
+            {
+                for (int i = 1; i < dex; i++)
+                {
+                    karyawan[i] = karyawan[i + 1];
+                }
+                dex--;
+                simpan();
+                return terHapus();
+            }
+            else if (hapus == 'n')
+            {
+                break;
+                return menu2();
+            }
+        case 2:
+//Mengedit data
+            cout << "\nEdit data:\n";
+            cout << "Nama: ";
+            cin >> karyawan[ind].nama;
+            cout << "Umur: ";
+            cin >> karyawan[ind].umur;
+            cout << "Gender: ";
+            cin >> karyawan[ind].jk;
+            cout << "Alamat: ";
+            cin >> karyawan[ind].alamat;
+            cout << "Status: ";
+            cin >> karyawan[ind].status;
+            cout << "Jabatan: ";
+            cin >> karyawan[ind].jabatan;
+            simpan();
+            bersih();
+            return terEdit();
+//Kembali ke menu
+        case 3:
+            bersih();
+            return menu2();
+        }
+    }
+    if (ketemu != true) //jika yang dicari tidak ada
+    {
+        return tidakValid();
+    }
 }
+
+char balik;
+
+void tidakValid()
+{
+    bersih();
+    kepala();
+    cout << "Data tidak ditemukan! [Error 404]\n";
+    cout << "Kembali ke menu pilihan? (y/n)";
+    cin >> balik;
+    if (balik == 'y')
+    {
+        bersih();
+        return menu2();
+    }
+}
+
+void terEdit()
+{
+    bersih();
+    kepala();
+    cout << "Data Berhasil Diedit!\n";
+    cout << "Kembali ke menu? (y/n)";
+    cin >> balik;
+    if (balik == 'y')
+    {
+        bersih();
+        return menu2();
+    }
+}
+
+void terHapus()
+{
+    bersih();
+    kepala();
+    cout << "Data Berhasil Dihapus!\n";
+    cout << "Kembali ke menu pilihan? (y/n)";
+    cin >> balik;
+    if (balik == 'y')
+    {
+        bersih();
+        return menu2();
+    }
+}
+
 
 void tambah() {
     bersih();
@@ -263,7 +421,7 @@ void tambah() {
             cin >> karyawan[i].nama;
             cout << "Umur: ";
             cin >> karyawan[i].umur;
-            cout << "Gender: ";
+            cout << "Jenis Kelamin: ";
             cin >> karyawan[i].jk;
             cout << "Alamat: ";
             cin >> karyawan[i].alamat;
